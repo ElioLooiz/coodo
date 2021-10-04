022d7767dabb717dc73cbceb3e527ea63f8c50b1
Vladislav Ovdeychuk <ovdeyvlad@gmail.com>
2021-10-04 21:43:48 +0300

    add send message

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const bot = new TG(process.env.BOT_TOKEN , { polling: true })
 
 bot.on('message', (msg) => {
     console.log(msg);
+    const text = msg.text;
+    const id = msg.chat.id;
+    bot.sendMessage(id, 'Лыч лох');
 });
